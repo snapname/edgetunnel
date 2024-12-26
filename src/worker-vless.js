d79f9cf840fdf2369be8632c3a311a940077c3a4
snapname <snapname@qq.com>
2024-12-26 21:33:31 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a9a4f08d-385a-4ba5-896e-7eaa21b214f8';
 
 let proxyIP = '';
 
